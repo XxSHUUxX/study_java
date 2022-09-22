4c95d642cc61fc79e5d57c70faac1aab67073098
Jinsook Kim <jim584800@gmail.com>
2022-09-23 00:19:43 +0900

    first commit.

---
+++ Study0923/Test2.java
@@ -0,0 +1,33 @@
+
+public class Test2 {
+	public static void main(String[] args) {
+
+		System.out.println('아');
+		System.out.println("아이티윌");
+//		System.out.println('아이티윌');
+		System.out.println("아");
+		System.out.println("");
+		
+		System.out.println("아이"+"티윌");
+		System.out.println(1+1);
+		System.out.println("1"+"1");
+		
+		System.out.println("선생님이 말했다 \"아! 자바 재밌다\"");
+		
+		System.out.println("선생님이 말했다");
+		System.out.println("\"아! 자바 재밌다!\"");
+		System.out.println("선생님이 말했다 \n \"아! 자바 재밌다!\"");
+		
+		System.out.print("아이");
+		System.out.print("티윌");
+		System.out.println();
+		
+		System.out.println("영어\t국어\t수학");
+		System.out.println("90\t80\t70");
+		
+		
+
+
+		
+}
+}
---
+++ Study0923/Test3.java
@@ -0,0 +1,12 @@
+
+public class Test3 {
+	public static void main(String[] args) {
+
+		int a=10000,b=1000;
+		System.out.println(a/b);
+		System.out.println(b*3);
+		System.out.println(a - (b * 5));
+		System.out.println((b * 5) * 0.01);
+		
+	}
+}
